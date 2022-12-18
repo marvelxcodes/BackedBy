27e9ddb0208435674d7bf1d80da79e72e6f25a3c
marvelxcodes <ramakrishnan0046@gmail.com>
2022-12-18 12:33:19 +0530

    Added Header

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/App.css'
+import Header from './components/Header'
+import Section1 from './components/Section1'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Section1 />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Section1.tsx
@@ -0,0 +1,13 @@
+import '../styles/section1.css'
+
+const Section1 = () => {
+  return (
+    <section className="s1">
+        <div className="card">
+          
+        </div>
+    </section>
+  )
+}
+
+export default Section1
