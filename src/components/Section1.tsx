f908b0855005a14442234aa169c533279e924ce2
marvelxcodes <ramakrishnan0046@gmail.com>
2022-12-18 15:20:49 +0530

    Added Card

--- src/components/Section1.tsx
+++ src/components/Section1.tsx
@@ -1,13 +1,25 @@
-import '../styles/section1.css'
+import "../styles/section1.css";
 
 const Section1 = () => {
   return (
     <section className="s1">
-        <div className="card">
-          
+      <div className="card">
+        <h1>Offer exclusive</h1>
+        <span>
+          <h1>content</h1>
+          <h3>powered by web3</h3>
+        </span>
+        <p>
+          <b>Offer your fans a monthly membership</b> for exclusive content with
+          the power of web3
+        </p>
+        <div className="">
+          <button>sign up</button>
+          <button>learn more</button>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Section1
+export default Section1;
