6d39a147d96493e7980f50ffad0be16790578cd1
marvelxcodes <ramakrishnan0046@gmail.com>
2022-12-20 12:50:18 +0530

    Finished Section 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './styles/App.css'
-import Header from './components/Header'
-import Section1 from './components/Section1'
-import Section2 from './components/Section2'
-import Section3 from './components/Section3'
-import Section4 from './components/Section4'
+import "./styles/App.css";
+import Header from "./components/Header";
+import Section1 from "./components/Section1";
+import Section2 from "./components/Section2";
+import Section3 from "./components/Section3";
+import Section4 from "./components/Section4";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
       <Section3 />
       <Section4 />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Section4.tsx
+++ src/components/Section4.tsx
@@ -1,21 +1,96 @@
-import '../styles/section4.css'
+import "../styles/section4.css";
 
 const Section4 = () => {
+  const Items: ItemType[] = [
+    {
+      quarter: 1,
+      year: 2022,
+      status: "completed",
+      title: "MVP Launched on Testnet",
+      description: `A functional version of BackedBy that allows creators to make profiles, mint $PROJECT tokens, setup tiers for membership access, and post content. Creators can also choose to list their $PROJECT tokens for sale. 
+        Individuals interested in directly investing in creators can become Backers by buying $PROJECT tokens. Subscribers can then choose a membership tier for purchase to access content. 
+        All revenue from membership purchases are distributed to $PROJECT token holders. All transactions are performed using the chain native token. `,
+    },
+    {
+      quarter: 2,
+      year: 2023,
+      status: "scheduled",
+      title: "MVP Launched on Testnet",
+      description: `A functional version of BackedBy that allows creators to make profiles, mint $PROJECT tokens, setup tiers for membership access, and post content. Creators can also choose to list their $PROJECT tokens for sale. 
+          Individuals interested in directly investing in creators can become Backers by buying $PROJECT tokens. Subscribers can then choose a membership tier for purchase to access content. 
+          All revenue from membership purchases are distributed to $PROJECT token holders. All transactions are performed using the chain native token. `,
+    },
+    {
+      quarter: 2,
+      year: 2022,
+      status: "scheduled",
+      title: "MVP Launched on Testnet",
+      description: `A functional version of BackedBy that allows creators to make profiles, mint $PROJECT tokens, setup tiers for membership access, and post content. Creators can also choose to list their $PROJECT tokens for sale. 
+          Individuals interested in directly investing in creators can become Backers by buying $PROJECT tokens. Subscribers can then choose a membership tier for purchase to access content. 
+          All revenue from membership purchases are distributed to $PROJECT token holders. All transactions are performed using the chain native token. `,
+    },
+  ];
   return (
-    <div className='s4'>
-        <div className="header">
-            <h3>Where are we going with this?</h3>
-            <h1>The BackedBy Roadmap</h1>
-        </div>
-        <div className="items-container">
-
-        </div>
-        <div className="btn-container">
-            <button className="btn-primary">SIGN UP</button>
-            <button className="btn-secondary">GET UPDATES</button>
-        </div>
+    <div className="s4">
+      <div className="header">
+        <h4>Where are we going with this?</h4>
+        <h1>The BackedBy Roadmap</h1>
+      </div>
+      <div className="items-container">
+        {Items.map((item) => (
+          <Item {...item} />
+        ))}
+      </div>
+      <div className="btn-container">
+        <button className="btn-primary">SIGN UP</button>
+        <button className="btn-secondary">GET UPDATES</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Section4
+type ItemType = {
+  quarter: number;
+  year: number;
+  status: "completed" | "scheduled";
+  title: string;
+  description: string;
+};
+
+export const Item = ({
+  quarter,
+  year,
+  status,
+  title,
+  description,
+}: ItemType) => {
+  return (
+    <div className="item">
+      <div className="header">
+        <div
+          className={`timestamp timestamp-${
+            status == "completed" ? "completed" : "scheduled"
+          }`}
+        >
+          <h1>Q{quarter}</h1>
+          <h3>{year}</h3>
+        </div>
+        <div className="title-bar">
+          <h1 className="title">{title}</h1>
+          <h4
+            className={`status status-${
+              status == "completed" ? "completed" : "scheduled"
+            }`}
+          >
+            {status}
+          </h4>
+        </div>
+      </div>
+      <div className="description">
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Section4;
